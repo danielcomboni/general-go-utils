b65e730bba1629fe31091829228295301a32de19
Daniel Comboni <comboni93@gmail.com>
2022-12-03 16:01:16 +0300

    add case insensitive field/property presence test

--- general_utils_fields.go
+++ general_utils_fields.go
@@ -4,11 +4,36 @@
 
 package general_goutils
 
-import "reflect"
+import (
+	"reflect"
+	"strings"
+)
 
-// HasField returns true if the struct [T] has the field specified by `fieldName` 
+// HasField returns true if the struct [T] has the field specified by `fieldName`. This is a case sensitive test
 func HasField[T any](fieldName string) bool {
 	metaValue := reflect.ValueOf(new(T)).Elem()
-	field := metaValue.FieldByName(fieldName)
+	field := metaValue.FieldByName(fieldName)	
 	return field != (reflect.Value{})
+}
+
+// GetFieldValue returns the value of the specified field/property of the struct t
+func GetFieldValue[T any](t *T,field string) interface{} {
+    r := reflect.ValueOf(t)
+    f := reflect.Indirect(r).FieldByName(field)
+    return f
+}
+
+// HasField_CaseInsensitive returns true if the struct [T] has the field specified by `fieldName`. This is a case insensitive test
+func HasField_CaseInsensitive[T any](fieldName string) bool {
+	var t T
+	v := reflect.ValueOf(t)
+	typeOfS := v.Type()
+
+    for i := 0; i< v.NumField(); i++ {
+		if strings.EqualFold(typeOfS.Field(i).Name,fieldName) {
+			return true
+		}
+    }
+
+	return false
 }
--- general_utils_fields_test.go
+++ general_utils_fields_test.go
@@ -1,10 +1,12 @@
 /**
  * Author: Daniel Comboni
  */
- package general_goutils
+package general_goutils
 
-
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type Test struct {
 	Name string
@@ -13,5 +15,20 @@ type Test struct {
 
 func TestHasField(t *testing.T) {
 	println(HasField[Test]("Name")) // prints true
-	println(HasField[Test]("Non")) // prints false
+	println(HasField[Test]("name")) // prints false
+}
+
+func TestGetFieldValue(t *testing.T) {
+	v := Test{
+		Name: "Daniel",
+		Age: 99,
+	}
+
+	u := GetFieldValue(&v,"Name")
+	println(fmt.Sprintf("place: %v", u))
+
+	println()
+
+	println(HasField_CaseInsensitive[Test]("name"))
+
 }
